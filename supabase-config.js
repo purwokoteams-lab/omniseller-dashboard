d9fb4231784985628e0ba6e267f9673d263f4c42
purwokoteams-lab <purwoko.teams@gmail.com>
2026-06-28 09:17:56 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,9 @@
 // ===== KONFIGURASI SUPABASE =====
 // Project URL & Anon Key OmniSeller
-const SUPABASE_URL = 'https://lbwmmppaunqikpollvhg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxid21tcHBhdW5xaWtwb2xsdmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NTk3NzAsImV4cCI6MjA5ODAzNTc3MH0.O-tEKALTaAwAgVRnC3CKfDxAFuqq8-f43Mlmr7X6F3s';
+const SUPABASE_URL = 'https://kjfqyirzqoetbbrnnkbi.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZnF5aXJ6cW9ldGJicm5ua2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NjczMzIsImV4cCI6MjA5ODE0MzMzMn0.i6pmZKxwaegdM9wAGn8z8C9Pj98GAgEmq81l5esHcSY
+
+';
 
 // Inisialisasi client Supabase (tersedia secara global sebagai `supabaseClient`)
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
