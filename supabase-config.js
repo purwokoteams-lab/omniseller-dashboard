e1b4a7fe5a99ca80ba098c80e693ba53c67855db
purwokoteams-lab <purwoko.teams@gmail.com>
2026-06-28 08:48:07 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // ===== KONFIGURASI SUPABASE =====
 // Project URL & Anon Key OmniSeller
-const SUPABASE_URL = 'https://kjfqyirzqoetbbrnnkbi.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://kjfqyirzqoetbbrnnkbi.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZnF5aXJ6cW9ldGJicm5ua2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NjczMzIsImV4cCI6MjA5ODE0MzMzMn0.i6pmZKxwaegdM9wAGn8z8C9Pj98GAgEmq81l5esHcSY';
 
 // Inisialisasi client Supabase (tersedia secara global sebagai `supabaseClient`)
